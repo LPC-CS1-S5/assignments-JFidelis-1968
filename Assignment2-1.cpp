149d2847f09c0ea87ff6155e25b970a702bbdafd
JFidelis <jan.k.fidelis@gmail.com>
2021-01-25 19:36:10 +0000

    submit 2-1, submit 2-2

---
+++ Assignment2-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int checking ; 
+	unsigned int miles;
+	long days;
+
+	checking = -20;
+	miles = 4276;
+	days = 18900;
+	cout << "We have made a long journey of " << miles << "miles.\n";
+	cout << "Our checking account balance is " << checking << "\n"; // new line
+	cout << "About " << days << " days ago Columbus stood on this spot.\n";
+	return 0;
+
+    // test comment
+
+}
---
+++ Assignment2-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter;
+
+    letter = 70;
+    cout << letter;
+
+    letter = 73;
+    cout << letter;
+
+    letter = 68;
+    cout << letter;
+
+    letter = 69;
+    cout << letter;
+
+    letter = 76;
+    cout << letter;
+
+    letter = 73;
+    cout << letter;
+
+    letter = 83;
+    cout << letter << "\n";
+    
+    return 0;
+}
