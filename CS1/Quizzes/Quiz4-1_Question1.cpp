36025a2dd006e9e9e812b62de992a6687d82176d
JFidelis <jan.k.fidelis@gmail.com>
2021-02-23 19:16:44 +0000

    Quiz 4-1 question 1

--- CS1/Quizzes/Quiz4-1_Question1.cpp
+++ CS1/Quizzes/Quiz4-1_Question1.cpp
@@ -27,23 +27,47 @@ int main()
         case 1:
             cout << "Please enter the radius of the circle:" << endl;
             cin >> radius;
-            circle = 3.14159 * pow( radius, 2 );
-            cout << setprecision(5) << fixed;
-            cout << "The area of the circle is " << circle << "." << endl;
+            if ( radius < 0 )
+            {
+                cout << "Invalid input, exiting program" << endl;
+                exit(0);
+            }
+            else
+            {
+                circle = 3.14159 * pow( radius, 2 );
+                cout << setprecision(5) << fixed;
+                cout << "The area of the circle is " << circle << "." << endl;
+            }
             break;
         case 2:
             cout << "Please enter the length and width of the rectangle:" << endl;
             cin >> length >> width;
-            rectangle = length * width;
-            cout << setprecision(2) << fixed;
-            cout << "The area of the rectangle is " << rectangle << "." << endl;
+            if ( length < 0 || width < 0)
+            {
+                cout << "Invalid input, exiting program" << endl;
+                exit(0);
+            }
+            else
+            {
+                rectangle = length * width;
+                cout << setprecision(2) << fixed;
+                cout << "The area of the rectangle is " << rectangle << "." << endl;
+            }
             break;
         case 3:
             cout << "Please enter the base and height of the triangle" << endl;
             cin >> base >> height;
-            triangle = ( base * height ) / 2;
-            cout << setprecision(2) << fixed;
-            cout << "The area of the triangle is " << triangle << "." << endl;
+            if ( base < 0 || height < 0 )
+            {
+                cout << "Invalid input, exiting program" << endl;
+                exit(0);
+            }
+            else
+            {
+                triangle = ( base * height ) / 2;
+                cout << setprecision(2) << fixed;
+                cout << "The area of the triangle is " << triangle << "." << endl;
+            }
             break;
         case 4:
             cout << "Exiting program." << endl;
