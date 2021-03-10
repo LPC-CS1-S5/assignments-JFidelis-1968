35ac3468076ab89a4742e1f405463de9b9740410
JFidelis <jan.k.fidelis@gmail.com>
2021-03-10 16:57:57 +0000

    created input validation for Quiz5-2 Question 2.

--- CS1/Quizzes/Quiz5-2_Question2.cpp
+++ CS1/Quizzes/Quiz5-2_Question2.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main()
 {
+  int N, M;
+  int pow, exp;
   
+  cout << "Enter the base 'N' and the exponent 'M'. They must be greater than 0 and less than 10:" << endl;
+  cin >> N >> M;
+
+  while ( ( N < 1 || N > 10 ) && ( M < 1 || M > 10 ) )
+  {
+    cout << "Please try again, the integers must be greater than 0 and less than 10." << endl;
+    cin >> N >> M;
+  }
+
 }
