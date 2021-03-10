387683e32367f6fdc6c85309d36caf666859f48b
JFidelis <jan.k.fidelis@gmail.com>
2021-03-10 16:33:19 +0000

    Quiz5-2 Question 1, updated and initialized the min value and created if statement to fin min.

--- CS1/Quizzes/Quiz5-2_Question1.cpp
+++ CS1/Quizzes/Quiz5-2_Question1.cpp
@@ -6,14 +6,25 @@ using namespace std;
 int main()
 {
   int rnum, i;
+  int min;
+
+  min = 100;
+  i = 0;
 
   srand(time(0));
 
-  while ( i < 6 )
+  cout << "Random numbers generated:" << endl;
+
+  while ( i < 5 )
   {
     rnum = rand() % 100;
-    cout << rnum << " was generated randomly" << endl;
-    i++;
+    cout << rnum << endl;
+    if ( min > rnum )
+      min = rnum;
+    i+= 1;
   }
 
+  cout << "The minimum value is " << min << endl;
+
+  return 0;
 }
