0c56ab81eaacbf1cc35002982c4f3748dab70632
JFidelis <jan.k.fidelis@gmail.com>
2021-03-10 17:57:08 +0000

    Created write file for Quiz5-2 Question 4.

--- CS1/Quizzes/Quiz5-2_Question4-1.cpp
+++ CS1/Quizzes/Quiz5-2_Question4-1.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main()
 {
-  
+  ofstream ofs;
+  int num_employees;
+  int employee_ID;
+  string employee_name, employee_department;
+  double employee_salary;
+
+  cout << "Enter the number of employees:";
+  cin >> num_employees;
+  cout << endl;
+  cout << "Enter the employee ID, name, department, and salary:" << endl;
+  cin >> employee_ID >> employee_name >> employee_department >> employee_salary;
+
+  ofs.open("employee.txt");
+
+  if (ofs.fail() )
+  {
+    cerr << "File Open Error";
+    exit(0);
+  }
+
+  ofs.close();
+
+  return 0;
 }
