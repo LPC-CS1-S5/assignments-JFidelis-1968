c8051fa8f1757e0e5854eacb922b82559f02154b
JFidelis <jan.k.fidelis@gmail.com>
2021-03-10 18:12:42 +0000

    Corrected formatting in write file for Quiz5-2 Question 4.

--- CS1/Quizzes/Quiz5-2_Question4-1.cpp
+++ CS1/Quizzes/Quiz5-2_Question4-1.cpp
@@ -28,19 +28,19 @@ int main()
   {
     cout << "Enter the employee's ID:" << endl;
     cin >> employee_ID;
-    ofs << employee_ID;
+    ofs << employee_ID << " ";
 
     cout << "Enter the employee's name:" << endl;
     cin >> employee_name;
-    ofs << employee_name;
+    ofs << employee_name << " ";
     
     cout << "Enter the department:" << endl;
     cin >> employee_department;
-    ofs << employee_department;
+    ofs << employee_department << " ";
 
     cout << "Enter the employee's salary:" << endl; 
     cin >> employee_salary;
-    ofs << employee_salary;
+    ofs << employee_salary << endl;
 
     i++;
   }
