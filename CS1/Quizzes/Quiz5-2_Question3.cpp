8ab67f0c5278029036095e6d79004744da61f617
JFidelis <jan.k.fidelis@gmail.com>
2021-03-10 17:18:51 +0000

    Initialized variable in Quiz5-2 Question 3.

--- CS1/Quizzes/Quiz5-2_Question3.cpp
+++ CS1/Quizzes/Quiz5-2_Question3.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main()
 {
-  
+  int num;
+  int i;
+  int start, end;
+
+  cout << "Please enter a starting value and an ending value" << endl;
+  cin >> start >> end;
+
 }
