f1fcd7365527583163cc3eebcd209ede849d8d04
JFidelis <jan.k.fidelis@gmail.com>
2021-03-31 17:04:09 +0000

    question 3 finished

--- CS1/Midterm/Question_3.cpp
+++ CS1/Midterm/Question_3.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int get_random(void);
 
-int is_greater(int n);
+int is_greater(int);
 
 int main()
 {
   ofstream ofs;
-  int n = 0, num = 0, N = 0;
+  int num, N = 0;
 
   unsigned seed = time(0);
   srand(seed);
@@ -26,9 +26,10 @@ int main()
 
   while (N < 10)
   {
-    get_random();
+    num = get_random();
     if (is_greater(num))
-      ofs << n;
+      ofs << num << endl;
+    cout << num << endl;
     N++;
   }
 
@@ -43,7 +44,15 @@ int get_random(void)
 
 int is_greater(int num)
 {
-  if (num > get_random())
+  static int prev = 0;
+
+  if (prev == 0)
+  {
+    prev = num;
+    return 0;
+  }
+
+  if (num > prev)
     return 1;
   else
     return 0;
