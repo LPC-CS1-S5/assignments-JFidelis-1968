849359fe2da003a3d8b75b403334ad279ef4c829
JFidelis <jan.k.fidelis@gmail.com>
2021-03-31 16:03:30 +0000

    question 3 progress

--- CS1/Midterm/Question_3.cpp
+++ CS1/Midterm/Question_3.cpp
@@ -2,7 +2,18 @@
 #include <fstream>
 using namespace std;
 
+void get_random(int n);
+
+int is_greater(int n);
+
 int main()
 {
-  
+  ofstream ofs;
+  int N = 10;
+
+  ofs.open("numbers.txt");
+
+  get_random
+
+
 }
