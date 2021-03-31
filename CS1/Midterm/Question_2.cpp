72b44c6ac3142d54e61c2d010bd17875abab6646
JFidelis <jan.k.fidelis@gmail.com>
2021-03-31 15:44:14 +0000

    Question 2 progress

--- CS1/Midterm/Question_2.cpp
+++ CS1/Midterm/Question_2.cpp
@@ -16,5 +16,14 @@ int main()
     cerr << "File Read Error" << endl;
   }
 
+  while (ifs >> student_name >> score1 >> score2)
+  {
+    cout << "Student name: " << student_name << endl;
+    cout << "Score 1: " << score1 << " " << "Score 2: " << score2 << endl;
+    sum = score1 + score2;
+    average = sum / 2;
+    cout << "Average: " << average << endl;
+  }
 
+  ifs.close();
 }
