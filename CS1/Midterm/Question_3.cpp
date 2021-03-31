af2de71ff5f67b95e4d22d063ef0617162ceca03
JFidelis <jan.k.fidelis@gmail.com>
2021-03-31 16:16:12 +0000

    question 3 progress

--- CS1/Midterm/Question_3.cpp
+++ CS1/Midterm/Question_3.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void get_random(int n);
 
-int is_greater(int n);
+//int is_greater(int n);
 
 int main()
 {
   ofstream ofs;
-  int N = 10;
+  int n, N = 10;
 
   ofs.open("numbers.txt");
 
-  get_random
+  if(ofs.fail())
+  {
+    cerr << "File Open Error";
+    exit(0);
+  }
 
+  while (N < 10)
+  {
+    get_random(n);
+    cout << n << endl;
+    //is_greater(n);
+    N++;
+  }
 
+  ofs.close();
+  return 0;
+}
+
+void get_random(int n)
+{
+  unsigned seed = time(0);
+  srand(seed);
+
+  n = rand() % 50;
 }
