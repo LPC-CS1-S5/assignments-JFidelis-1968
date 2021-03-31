3024e608f50979bf46cd6db832bd937adb0701c7
JFidelis <jan.k.fidelis@gmail.com>
2021-03-31 15:38:11 +0000

    Question 2 progress

--- CS1/Midterm/Question_2.cpp
+++ CS1/Midterm/Question_2.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-  
+  ifstream ifs;
+
+  int score1, score2, sum, average;
+  string student_name;
+
+  ifs.open("students.txt");
+
+  if (ifs.fail())
+  {
+    cerr << "File Read Error" << endl;
+  }
+
+
 }
