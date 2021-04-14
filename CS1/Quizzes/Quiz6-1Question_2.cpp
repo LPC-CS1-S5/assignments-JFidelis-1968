e3b526b22ba4443383498866e4961f491ae0e0cb
JFidelis <>
2021-04-14 00:57:20 +0000

    Quiz6-1 Question 2 progress

--- CS1/Quizzes/Quiz6-1Question_2.cpp
+++ CS1/Quizzes/Quiz6-1Question_2.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-
+void getRandnum(int &n1, int &n2, int &n3);
+int findMin(int n1, int n2, int n3);
+void printResult(int n1, int n2, int n3, int min);
 
 int main()
 {
+  int n1, n2, n3;
+  int min;
+
+  getRandnum(n1, n2, n3);
+  min = findMin(n1, n2, n3);
+  printResult(n1, n2, n3, min);
+}
+
+void getRandnum(int &n1, int &n2, int &n3)
+{
   
+
+  n1
+}
+
+int findMin(int n1, int n2, int n3)
+{
+
+}
+
+void printResult(int n1, int n2, int n3)
+{
+
 }
 
