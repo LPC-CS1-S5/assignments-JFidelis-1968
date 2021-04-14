7984a672831bdc67eeda58aae0987655b3236268
JFidelis <>
2021-04-14 01:06:34 +0000

    Quiz6-1 Question 2 done

--- CS1/Quizzes/Quiz6-1Question_2.cpp
+++ CS1/Quizzes/Quiz6-1Question_2.cpp
@@ -19,18 +19,32 @@ int main()
 
 void getRandnum(int &n1, int &n2, int &n3)
 {
-  
+  srand(time(0));
 
-  n1
+  n1 = rand() % 100;
+  n2 = rand() % 100;
+  n3 = rand() % 100;
 }
 
 int findMin(int n1, int n2, int n3)
 {
+  int min = 100;
 
+  if(min > n1)
+   min = n1;
+  if(min > n2)
+   min = n2;
+  if(min > n3)
+   min = n3;
+
+  return min;
 }
 
-void printResult(int n1, int n2, int n3)
+void printResult(int n1, int n2, int n3, int min)
 {
-
+  cout << "The random numbers are: " << endl;
+  cout << n1 << " " << n2 << " " << n3 << endl;
+  cout << "The minimum value is: " << endl;
+  cout << min << endl;
 }
 
