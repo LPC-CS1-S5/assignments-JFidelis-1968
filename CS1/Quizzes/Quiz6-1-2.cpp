ae0817d1b777a3e50e6f207f17e1b4f4822570fb
JFidelis <>
2021-04-14 00:31:44 +0000

    Quiz6-1 progress

---
+++ CS1/Quizzes/Quiz6-1-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swapTwoValues(int n1, int n2);
+void swapTwoValues(float f1, float f2);
+
+int main()
+{
+
+}
+
+void swapTwoValues(int n1, int n2)
+{
+
+}
+
+void swapTwoValues(float f1, float f2)
+{
+  
+}
