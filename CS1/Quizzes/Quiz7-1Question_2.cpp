57f42117ed91ebcb4530e85da2fba112349b9e40
JFidelis <>
2021-05-04 17:13:01 +0000

    created nested for loop

--- CS1/Quizzes/Quiz7-1Question_2.cpp
+++ CS1/Quizzes/Quiz7-1Question_2.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void printtriangle(int [][3], int)
+void printtriangle(int [][3], int);
 
 int main()
 {
     const int SIZE = 3;
-    int numbers[SIZE][SIZE] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}}
+    int numbers[SIZE][SIZE] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
 
     printtriangle(numbers, SIZE);
 }
 
 void printtriangle(int numbers[][3], int SIZE)
 {
-  
+    for(int i = 0; i < SIZE; i++)
+    {
+      for(int j = 0; j < SIZE; j++)
+      {
+
+      }
+    } 
 }
