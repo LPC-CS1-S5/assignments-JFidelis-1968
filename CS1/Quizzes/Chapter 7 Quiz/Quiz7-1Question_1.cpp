51f1240be4a808017c0b8b7c76b3574a6604dd14
JFidelis <>
2021-05-04 15:45:12 +0000

    changed function definitions

--- CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
+++ CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-void checkNum(int);
+void checkNum(int [], int);
 
 const int N = 10;
 
@@ -10,17 +10,17 @@ int main()
 {
   int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
 
-  int n = 0;
+  int absval = 0;
 
   for(int i = 0; i < N; i++)
   {
-    checkNum(n, i);
+    absval = checkNum(number[], i);
     cout << number[i];
   }
 }
 
-void checkNum(int n, int i)
+void checkNum(int number[], int i)
 {
-  if (abs(n[i] - n[i + 1]) < 10)
+  if (abs(number[i] - number[i + 1]) < 10)
     cout << "The number is less than 10" << endl;
 }
