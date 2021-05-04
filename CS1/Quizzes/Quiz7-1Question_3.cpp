509cde96cec29359ff0087b6b851def6d73e7474
JFidelis <>
2021-05-04 17:41:34 +0000

    created findMax function

--- CS1/Quizzes/Quiz7-1Question_3.cpp
+++ CS1/Quizzes/Quiz7-1Question_3.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int findMax(int [], int, int);
+
 int main()
 {
    const int N = 10;
-   int number[N] {10, 15, 0, -25, 19, 20, 25, 30, 45, 20}; 
+   int numbers[N] {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+
+
+}
+
+int findMax(int numbers[], int begin, int end)
+{
+  
 }
