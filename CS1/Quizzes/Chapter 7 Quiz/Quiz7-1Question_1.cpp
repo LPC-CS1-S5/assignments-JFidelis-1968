2ced2697ad757dc140834d03a0b1f3dc5388bfb0
JFidelis <>
2021-05-04 15:36:16 +0000

    working through array

--- CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
+++ CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
@@ -2,19 +2,22 @@
 #include <cmath>
 using namespace std;
 
-void checkNum(int, int);
+void checkNum(int);
 
 const int N = 10;
 
 int main()
 {
   int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
-   
 
+  for(int i = 0; i < N; i++)
+  {
+    checkNum(number, i);
+  }
 }
 
-void checkNum(int n, int i)
+void checkNum(int number[i])
 {
-  if (abs(n[i] - n[i + 1]) < 10)
+  if (abs(number[i] - number[i + 1]0) < 10)
     cout << "The number is less than 10" << endl;
 }
