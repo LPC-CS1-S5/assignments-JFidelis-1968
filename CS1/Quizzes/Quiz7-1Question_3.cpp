9b43d45aaf5b64aa163467f6b3847e610dca71c9
JFidelis <>
2021-05-04 17:48:26 +0000

    made if statement in findMax function

--- CS1/Quizzes/Quiz7-1Question_3.cpp
+++ CS1/Quizzes/Quiz7-1Question_3.cpp
@@ -5,13 +5,16 @@ int findMax(int [], int, int);
 
 int main()
 {
-   const int N = 10;
-   int numbers[N] {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+  const int N = 10;
+  int numbers[N] {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+  int max = 0;
 
 
 }
 
-int findMax(int numbers[], int begin, int end)
+int findMax(int numbers[], int max, int N)
 {
-  
+  if(max < numbers[N])
+    max = numbers[N];
+  return max;
 }
