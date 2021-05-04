a5e89d9f1ab81b6f67cc1a5a3659dca749eedd57
JFidelis <>
2021-05-04 17:10:50 +0000

    Created function 'printtriangle'

--- CS1/Quizzes/Quiz7-1Question_2.cpp
+++ CS1/Quizzes/Quiz7-1Question_2.cpp
@@ -7,4 +7,11 @@ int main()
 {
     const int SIZE = 3;
     int numbers[SIZE][SIZE] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}}
+
+    printtriangle(numbers, SIZE);
+}
+
+void printtriangle(int numbers[][3], int SIZE)
+{
+  
 }
