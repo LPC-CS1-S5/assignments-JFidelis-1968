7e93728741672f26fd6b358f8b13c124a7eafb20
JFidelis <>
2021-05-04 16:06:19 +0000

    debugging in VS code

--- CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
+++ CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
@@ -2,30 +2,31 @@
 #include <cmath>
 using namespace std;
 
-int checkNum(int [], int);
+void checkNum(int [], int, int);
 
 const int N = 10;
 
 int main()
 {
-  int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+    int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+    int absval = 0;
 
-  int absval = 0;
+    cout << "For array {10, 15, 0, -25, 19, 20, 25, 30, 45, 20} the following absolute value difference exist." << endl;
 
-  cout << "For array " << number[N] << " the following absolute value difference exist." << endl;
-
-  for(int i = 0; i < N; i++)
-  {
-    absval = checkNum(number, i);
-    if (absval == 1)
-      cout << number[i] << " Has an absolute value difference less than 10." << endl;
-  }
+    for(int i = 0; i < N; i++)
+    {
+     checkNum(number, i, absval);
+        if (absval == 1)
+        cout << number[i] << " Has an absolute value difference less than 10." << endl;
+    }
 }
 
-int checkNum(int number[], int i)
+void checkNum(int number[], int i, int absval)
 {
-  if (abs(number[i] - number[i + 1]) < 10)
-  {
-    return 1;
-  } 
+    int absval = 0;
+    if (abs(number[i] - number[i + 1]) < 10)
+    {
+        absval = 1;
+    }
+    return absval; 
 }
