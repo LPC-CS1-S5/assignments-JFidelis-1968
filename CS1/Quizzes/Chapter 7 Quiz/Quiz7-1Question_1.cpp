a9e90bbd51dd533c1e2471ad84b52f2209c9d93d
JFidelis <>
2021-05-04 15:50:51 +0000

    debugging

--- CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
+++ CS1/Quizzes/Chapter 7 Quiz/Quiz7-1Question_1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-void checkNum(int [], int);
+int checkNum(int [], int);
 
 const int N = 10;
 
@@ -14,13 +14,15 @@ int main()
 
   for(int i = 0; i < N; i++)
   {
-    absval = checkNum(number[], i);
-    cout << number[i];
+    absval = checkNum(number, i);
+    if (absval == 1)
+      cout << number[i] << endl;
   }
 }
 
-void checkNum(int number[], int i)
+int checkNum(int number[], int i)
 {
   if (abs(number[i] - number[i + 1]) < 10)
     cout << "The number is less than 10" << endl;
+  return 1;
 }
