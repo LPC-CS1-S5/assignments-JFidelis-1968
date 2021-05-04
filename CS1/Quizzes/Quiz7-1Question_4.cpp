105e56e4223b8a1ea6dbffac3adc058b998e1106
JFidelis <>
2021-05-04 19:02:21 +0000

    changed formatting and added in for loop to delete userinput

--- CS1/Quizzes/Quiz7-1Question_4.cpp
+++ CS1/Quizzes/Quiz7-1Question_4.cpp
@@ -12,37 +12,47 @@ void deleteOne(vector<int> &);
 
 int main()
 {
-  vector<int> number;
-  int size = 20;
+    vector<int> number;
+    int size = 20;
 
-  srand(time(0));
+    srand(time(0));
 
-  makeVector(number, size);
-  printVector(number);
-  deleteOne(number);   
+    makeVector(number, size);
+    printVector(number);
+    deleteOne(number);   
 }
 
 void makeVector(vector<int> &vec, int size)
 {
-  for(int i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
     vec.push_back(rand() % 10);
 }
 
 void printVector(vector<int> vec)
 {
-  for(int v : vec)
-    cout << v << "\t";
-  cout << endl;
+    for(int v : vec)
+    cout << v << " ";
+    cout << endl;
 }
 
 void deleteOne(vector<int> &vec)
 {
-   int usernum, deletecnt;
+    int usernum, deletecnt = 0;
+    vector<int>::iterator iter;
 
-   cout << "Please enter an integer between 1 and 10: " << endl;
-   cin >> usernum;
+    cout << "Please enter an integer between 1 and 10: " << endl;
+    cin >> usernum;
 
-   cout << usernum << " has been deleted " << deletecnt << " times." << endl;
+    for(iter = vec.begin(); iter < vec.end(); iter++)
+    {
+        if(usernum == *iter)
+        {
+            vec.erase(iter);
+            deletecnt += 1;
+        }
+    }
+    
+    cout << usernum << " has been deleted " << deletecnt << " times." << endl;
 
-   printVector(vec);
+    printVector(vec);
 }
