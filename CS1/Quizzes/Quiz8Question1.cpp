7c1c3bd3ddcc09f9012f150590ed67ec9384c688
JFidelis <>
2021-05-11 23:11:29 +0000

    variable declaration and file read

--- CS1/Quizzes/Quiz8Question1.cpp
+++ CS1/Quizzes/Quiz8Question1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
+const int N = 100;
+
 int main()
 {
+  int str1[N];
+  ifstream ifs;
 
+  ifs.open("python.txt");
 }
