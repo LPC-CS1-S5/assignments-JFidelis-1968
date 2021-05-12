1cef02b8c8e2be0415a24d7a624a0aa85ae69f28
JFidelis <>
2021-05-12 01:09:15 +0000

    Q8 Question 2 variable definitions

--- CS1/Quizzes/Quiz8Question2.cpp
+++ CS1/Quizzes/Quiz8Question2.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
+  string useremail;
+  
+  cout << "PLease enter a valid email address: " << endl;
+  cin >> useremail;
+
   
 }
