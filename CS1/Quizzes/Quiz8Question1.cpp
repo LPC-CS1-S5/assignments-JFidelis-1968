cbc6c1348f0ee1a84c6f48fe74a5a1031a28c0d0
JFidelis <>
2021-05-12 00:41:09 +0000

    changed traversal to nested loop structure to check for substring at every string element

--- CS1/Quizzes/Quiz8Question1.cpp
+++ CS1/Quizzes/Quiz8Question1.cpp
@@ -11,20 +11,20 @@ int main()
     int strcnt = 0;
 
     ifs.open("python.txt");
+    
 
     while(ifs >> str1)
     {
-        cout << str1 << endl;
-    }
-
-    int pos = str1.find(substr1, 0);
-
-    while(pos != string::npos)
-    {  
-        strcnt += 1;    
-        pos = str1.find(substr1, pos + 1);
+        int pos = str1.find(substr1, 0);
+        
+        while(pos != string::npos)
+        {  
+            strcnt += 1;    
+            pos = str1.find(substr1, pos + 1);
+        }
     }
 
     ifs.close();
 
     cout << "The substring 'th' can be found " << strcnt << " times in the paragraph." << endl;
+}
