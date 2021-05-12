b2d04242ddf802821c34282696d4813b04f11d7d
JFidelis <>
2021-05-12 01:17:29 +0000

    added for loop to count numbers and letters

--- CS1/Quizzes/Quiz8Question2.cpp
+++ CS1/Quizzes/Quiz8Question2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -8,6 +10,22 @@ int main()
   
   cout << "PLease enter a valid email address: " << endl;
   cin >> useremail;
+
+  int l = strlen(useremail);
+  int cnt_alpha = 0;
+  int cnt_digit = 0;
+
+  for(int i = 0; i < 30; i++)
+  {
+    if (isalpha(useremail[i]))
+    {
+      cnt_alpha++;
+    }
+    if (isdigit(useremail[i]))
+    {
+      cnt_digit++;
+    }
+  }
 
 
 }
