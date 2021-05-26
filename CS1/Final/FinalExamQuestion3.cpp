ac0d913de5fd76f7662155c35972ad565886ada7
JFidelis <>
2021-05-26 01:12:27 +0000

    fixed brackets and formatting

--- CS1/Final/FinalExamQuestion3.cpp
+++ CS1/Final/FinalExamQuestion3.cpp
@@ -8,35 +8,36 @@ int findSame(int [], int []);
 
 int main()
 {
-  int result;
-  int numbers1[SIZE1] = {2, 3, 5, 9, 10};
-  int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+    int result;
+    int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+    int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 
-  result = findSame(numbers1, numbers2);
+    result = findSame(numbers1, numbers2);
 
-  if(result == 1)
-    cout << "numbers1 is a subset of numbers 2." << endl;
-  else 
-    cout << "numbers1 is not a subset of numbers 2." << endl;
-
+    if(result == 1)
+        cout << "numbers1 is a subset of numbers 2." << endl;
+    else 
+        cout << "numbers1 is not a subset of numbers 2." << endl;
 }
 
 int findSame(int numbers1[], int numbers2[])
 {
-   int result = 0;
-   int elementSum = 0;
-   for(int i = 0; i < SIZE2; i++)
-   {
-    for(int j = 0; j < SIZE1; j++)
-      {
-        if(numbers2[i] == numbers1[j])
-          cout << "The array element " << j << " from numbers 2 is contained in array numbers1." << endl;
-          elementSum += 1;
-      }
-   }
-  if(elementSum == 5)
-    result = 1;
-  else
-    result = 0;
-  return result; 
+    int result = 0;
+    int elementSum = 0;
+    for(int i = 0; i < SIZE2; i++)
+    {
+        for(int j = 0; j < SIZE1; j++)
+        {
+            if(numbers2[i] == numbers1[j])
+            {
+                cout << "The array element " << numbers1[j] << " from numbers 2 is contained in array numbers1." << endl;
+                elementSum += 1;
+            }
+        }
+    }
+    if(elementSum == 5)
+        result = 1;
+    else
+        result = 0;
+    return result; 
 }
