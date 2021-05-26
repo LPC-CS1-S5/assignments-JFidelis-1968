6051337e604beefa390c46a798af497247181f13
JFidelis <>
2021-05-26 00:59:46 +0000

    created function 'findSame'

--- CS1/Final/FinalExamQuestion3.cpp
+++ CS1/Final/FinalExamQuestion3.cpp
@@ -4,8 +4,18 @@ using namespace std;
 const int SIZE1 = 5;
 const int SIZE2 = 10;
 
+int findSame(int [], int []);
+
 int main()
 {
+  int result;
   int numbers1[SIZE1] = {2, 3, 5, 9, 10};
   int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+  result = findSame(numbers1, numbers2);
+}
+
+int findSame(int numbers1[], int numbers2[])
+{
+    
 }
