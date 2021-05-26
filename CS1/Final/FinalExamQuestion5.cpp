004d378bb6436e8b4485a960aa462a8a3647e2c7
JFidelis <>
2021-05-26 20:51:46 +0000

    finished binWrite and fixed formatting

--- CS1/Final/FinalExamQuestion5.cpp
+++ CS1/Final/FinalExamQuestion5.cpp
@@ -7,74 +7,75 @@ const unsigned short N=5;
 const int NUM_STUDENTS = 16;
 
 struct Scores{     
-      double sc[N];     
-      string grade;
+    double sc[N];     
+    string grade;
 };
 
 struct Grade{    
-      string first;    
-      string last;   
-      string ssn;   
-      Scores score;
+    string first;    
+    string last;   
+    string ssn;   
+    Scores score;
 };
 
 void readFile(Grade []);
-void binWrite();
+void binWrite(Grade []);
 
 int main()
 {
-  Grade g[NUM_STUDENTS];
+    Grade g[NUM_STUDENTS];
 
-  readFile(g);
-  binWrite();
+    readFile(g);
+    binWrite(g);
 }
 
 void readFile(Grade g[])
 {
-  ifstream ifs;
-  Grade gr;
-  string readline;
+    ifstream ifs;
+    Grade gr;
+    string readline;
 
-  int pos;
+    int pos;
 
-  ifs.open("grades.txt");
+    ifs.open("grades.txt");
 
-  while(ifs >> readline)
-  {
-    pos = readline.find(',', pos + 1);
-    gr.first = readline.substr(0, pos);
-
-    int prev = pos + 1;
-    pos = readline.find(',', pos + 1);
-    gr.last = readline.substr(prev, pos - prev);
-
-    prev = pos + 1;
-    pos = readline.find(',', pos + 1);
-    gr.ssn = readline.substr(prev, pos - prev);
-    
-    for(int i = 0; i < N; i++)
+    while(ifs >> readline)
     {
+        pos = readline.find(',', pos + 1);
+        gr.first = readline.substr(0, pos);
+
+        int prev = pos + 1;
+        pos = readline.find(',', pos + 1);
+        gr.last = readline.substr(prev, pos - prev);
+
         prev = pos + 1;
-        pos = readline.find(',', pos + 1);        
-        gr.score.sc[i] = stod(readline.substr(prev, pos - prev));
+        pos = readline.find(',', pos + 1);
+        gr.ssn = readline.substr(prev, pos - prev);
+    
+        for(int i = 0; i < N; i++)
+        {
+            prev = pos + 1;
+            pos = readline.find(',', pos + 1);        
+            gr.score.sc[i] = stod(readline.substr(prev, pos - prev));
+        }
+
+        prev = pos + 1;
+        pos = readline.find(',', pos + 1);
+        gr.score.grade = readline.substr(prev, pos - prev);
+  
     }
 
-    prev = pos + 1;
-    pos = readline.find(',', pos + 1);
-    gr.score.grade = readline.substr(prev, pos - prev);
-  
-  }
-
-  ifs.close();
+    ifs.close();
 }
 
 void binWrite(Grade g[])
 {
-  ofstream ofs;
+    ofstream ofs;
+    Grade gr;
   
-  ofs.open("grades.bin");
+    ofs.open("grades.bin");
 
-  strcpy(g.first, ",");
+    ofs.write((char *)&gr, sizeof(gr));
 
-  ofs.close();
+    ofs.close();
 }
