020367bbfbac7c810020f47f5bd7228c6aab2bbb
JFidelis <>
2021-05-26 01:34:22 +0000

    started for loop

--- CS1/Final/FinalExamQuestion4.cpp
+++ CS1/Final/FinalExamQuestion4.cpp
@@ -7,11 +7,15 @@ const int stringSIZE = 250;
 int main()
 {
     string userinput;
-    string word = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini,qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
+    string words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini,qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
     cout << "Please enter the string you would like to find: " << endl;
     cin >> userinput;
 
-    for(int i = 0; i < )
+    for(int i = 0; i < stringSIZE; i++)
+    {
+      int pos = words.find(userinput, 0);
+    }
+
 
 }
