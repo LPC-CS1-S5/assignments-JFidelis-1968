57343e5fb5228718ed1245f43139b3fec5dd0bf0
JFidelis <>
2021-05-26 02:09:44 +0000

    Started Final Quetion 5, declared structs and variables.

--- CS1/Final/FinalExamQuestion5.cpp
+++ CS1/Final/FinalExamQuestion5.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+const unsigned short N=5;
+struct Scores{     
+      double sc[N];     
+      string grade;
+};
+
+struct Grade{    
+      string first;    
+      string last;   
+      string ssn;   
+      Scores score;
+};
+
 int main()
 {
-    
+  Grade g;
 }
